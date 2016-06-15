93089e3d01f1d3fcde12f650e43eedda2a36656a
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-06-16 04:06:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+export { default as TabView } from './src/TabView';
+export { default as TabBarTop } from './src/TabBarTop';
+export { default as TabViewPropTypes } from './src/TabViewPropTypes';
+export { default as TabViewTypes } from './src/TabViewTypes';
