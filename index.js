4a553fb2b82fc8a8e0bce797e0c8701f6948a3f5
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-06-18 19:48:39 +0530

    breaking: rework API to be able to specify custom panHandlers

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
+export { default as TabViewTransitioner } from './src/TabViewTransitioner';
 export { default as TabView } from './src/TabView';
-export { default as TabViewAnimated } from './src/TabViewAnimated';
 export { default as TabBarTop } from './src/TabBarTop';
-export { default as TabViewPropTypes } from './src/TabViewPropTypes';
-export { default as TabViewTypes } from './src/TabViewTypes';
