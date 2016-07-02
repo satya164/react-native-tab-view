e979305e4aedc10315c71eb993b6233c5814f855
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-07-02 23:33:04 +0530

    refactor: add a basic TabBar component

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { default as TabViewTransitioner } from './src/TabViewTransitioner';
 export { default as TabViewAnimated } from './src/TabViewAnimated';
 export { default as TabViewPage } from './src/TabViewPage';
+export { default as TabBar } from './src/TabBar';
 export { default as TabBarTop } from './src/TabBarTop';
