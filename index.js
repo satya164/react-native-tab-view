27e6ce5ffaf077ba8bcacafecc61ece27133f5d9
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-10-11 21:55:37 +0530

    Add ability to use alternative pager components (#36)
    * breaking: remove <TabViewPage /> and introduce a pager component
    
    * feat: add a ScrollView based pager
    
    * feat: add a subscribe prop to subscribe to index and position
    
    * refactor: don't subscribe to position directly
    
    * refactor: pass children as an array
    
    * feat: add a ViewPagerAndroid based pager
    
    * fix: set tab index after page change
    
    * refactor: rename pagers
    
    * feat: add a 'swipeDistanceThreshold' and 'swipeVelocityThreshold' prop
    
    * docs: add alternative pagers to the docs
    
    * fix: respect swipeEnabled prop
    
    * fix: reset next page after jump
    
    * fix: don't skip animating position
    
    * fix: avoid jumps when transitioning
    
    * chore: fix flow errors
    
    * fix: actually use the 'swipeDistanceThreshold' prop
    
    * docs: remove unnecessary width
    
    * fix: fix removing subscription
    
    * fix: don't disable animations when 'swipeEnabled' is false
    
    * fix: reset next page when jumped
    
    * fix: add check for refs to prevent crash
    
    * fix: rework ViewPagerAndroid based pager
    
    * docs: remove indicator from NoAnimationExample
    
    * docs: remove some examples
    
    * fix: update scroll value on layout or children change

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+/* @flow */
+
 export { default as TabViewTransitioner } from './src/TabViewTransitioner';
 export { default as TabViewAnimated } from './src/TabViewAnimated';
-export { default as TabViewPage } from './src/TabViewPage';
+export { default as TabViewPagerPan } from './src/TabViewPagerPan';
+export { default as TabViewPagerScroll } from './src/TabViewPagerScroll';
+export { default as TabViewPagerAndroid } from './src/TabViewPagerAndroid';
 export { default as TabBar } from './src/TabBar';
 export { default as TabBarTop } from './src/TabBarTop';
