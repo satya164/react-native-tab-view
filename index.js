2a40380fcc22d4052a93f981d263d8e225e8d9a2
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-10-11 22:21:53 +0530

    fix: add a <TabViewPage /> component with deprecation warning

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 export { default as TabViewTransitioner } from './src/TabViewTransitioner';
 export { default as TabViewAnimated } from './src/TabViewAnimated';
+export { default as TabViewPage } from './src/TabViewPage';
 export { default as TabViewPagerPan } from './src/TabViewPagerPan';
 export { default as TabViewPagerScroll } from './src/TabViewPagerScroll';
 export { default as TabViewPagerAndroid } from './src/TabViewPagerAndroid';
