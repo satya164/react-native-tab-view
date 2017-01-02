ccaf1a7f25430b74a06fc9158a4c561ea4c1d2c2
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-01-03 02:25:03 +0530

    refactor: export modules as getters

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /* @flow */
+/* eslint-disable import/no-commonjs */
 
-export { default as TabViewTransitioner } from './src/TabViewTransitioner';
-export { default as TabViewAnimated } from './src/TabViewAnimated';
-export { default as TabViewPagerPan } from './src/TabViewPagerPan';
-export { default as TabViewPagerScroll } from './src/TabViewPagerScroll';
-export { default as TabViewPagerAndroid } from './src/TabViewPagerAndroid';
-export { default as TabBar } from './src/TabBar';
-export { default as TabBarTop } from './src/TabBarTop';
+module.exports = {
+  get TabViewTransitioner() { return require('./src/TabViewTransitioner').default; },
+  get TabViewAnimated() { return require('./src/TabViewAnimated').default; },
+  get TabViewPagerPan() { return require('./src/TabViewPagerPan').default; },
+  get TabViewPagerScroll() { return require('./src/TabViewPagerScroll').default; },
+  get TabViewPagerAndroid() { return require('./src/TabViewPagerAndroid').default; },
+  get TabBar() { return require('./src/TabBar').default; },
+  get TabBarTop() { return require('./src/TabBarTop').default; },
+};
