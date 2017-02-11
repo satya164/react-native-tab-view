90bf9b5a15bcab894f317e7ff958693725018469
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-02-11 19:07:38 +0530

    chore: add tests

---
+++ __tests__/TabViewPagerScroll.test.js
@@ -0,0 +1,83 @@
+/* @flow */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import {
+  Animated,
+  View,
+} from 'react-native';
+import TabViewPagerScroll from '../src/TabViewPagerScroll';
+
+it('renders only focused child until layout', () => {
+  /* $FlowFixMe */
+  console.error = jest.fn(); // suppress propType warnings
+
+  const component = shallow(
+    <TabViewPagerScroll
+      layout={{ height: 0, width: 0, measured: false }}
+      navigationState={{
+        index: 1,
+        routes: [
+          { key: 'first', testID: 'first' },
+          { key: 'second', testID: 'second' },
+          { key: 'third', testID: 'third' },
+        ],
+      }}
+      position={new Animated.Value(1)}
+      jumpToIndex={jest.fn()}
+      getLastPosition={jest.fn()}
+      subscribe={jest.fn()}
+    >
+      <View />
+      <View />
+      <View />
+    </TabViewPagerScroll>
+  );
+
+  expect(component.children().length).toBe(1);
+  expect(component.find({ testID: 'first' }).length).toBe(0);
+  expect(component.find({ testID: 'second' }).length).toBe(1);
+  expect(component.find({ testID: 'third' }).length).toBe(0);
+
+  component.setProps({
+    layout: { height: 320, width: 240, measured: false },
+  });
+
+  expect(component.children().length).toBe(3);
+  expect(component.find({ testID: 'first' }).length).toBe(1);
+  expect(component.find({ testID: 'second' }).length).toBe(1);
+  expect(component.find({ testID: 'third' }).length).toBe(1);
+});
+
+it('sets initial scroll position according to navigation state index', () => {
+  /* $FlowFixMe */
+  console.error = jest.fn(); // suppress propType warnings
+
+  const component = shallow(
+    <TabViewPagerScroll
+      layout={{ height: 320, width: 240, measured: false }}
+      navigationState={{
+        index: 2,
+        routes: [
+          { key: 'first', testID: 'first' },
+          { key: 'second', testID: 'second' },
+          { key: 'third', testID: 'third' },
+        ],
+      }}
+      position={new Animated.Value(1)}
+      jumpToIndex={jest.fn()}
+      getLastPosition={jest.fn()}
+      subscribe={jest.fn()}
+    >
+      <View />
+      <View />
+      <View />
+    </TabViewPagerScroll>
+  );
+
+  /* $FlowFixMe */
+  expect(component.dive().instance().props.contentOffset).toEqual({
+    x: 240 * 2,
+    y: 0,
+  });
+});
