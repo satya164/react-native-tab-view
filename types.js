928ac4bfe17031529dce435bd07045bb0270e04d
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-04-25 04:14:10 +0530

    fix: improved flowtype support

---
+++ types.js
@@ -0,0 +1,6 @@
+/* @flow */
+
+export type { Route } from './src/TabViewTypeDefinitions';
+export type { Scene } from './src/TabViewTypeDefinitions';
+export type { NavigationState } from './src/TabViewTypeDefinitions';
+export type { SceneRendererProps } from './src/TabViewTypeDefinitions';
