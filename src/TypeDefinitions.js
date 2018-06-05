2963fc8f7872a01dff341047ecddb250ea5a25a4
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-06-05 11:38:57 +0200

    Update TypeDefinitions.js

--- src/TypeDefinitions.js
+++ src/TypeDefinitions.js
@@ -1,8 +1,8 @@
 /* @flow */
 
+import * as React from 'react';
 import { Animated } from 'react-native';
 import type { Node } from 'react';
-import * as React from 'react';
 
 export type Route<T: { key: string }> = $Exact<T>;
 
