9adf3b9cd0e887328708fe9a1e8e3b3e3f165ad7
NoemiRozpara <nrozpara@gmail.com>
2019-10-25 15:37:31 +0200

    WIP: config

--- example/src/NativePagerExample.tsx
+++ example/src/NativePagerExample.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Stylesheet, View, Text } from 'react-native';
 
 export default function MyPager() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ export { default as TabView } from './TabView';
 export { default as TabBar } from './TabBar';
 export { default as SceneMap } from './SceneMap';
 
-export const ViewPager = require('@react-native-community/viewpager');
+export { default as ViewPager } = require('@react-native-community/viewpager');
 
 export { Route, NavigationState, SceneRendererProps } from './types';
