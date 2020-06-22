8b13efff74a7a159acd120850ef3bbee650e31aa
osdnk <micosa97@gmail.com>
2020-06-22 19:51:14 +0200

    fix: uncomment important line

--- src/ScrollPager.tsx
+++ src/ScrollPager.tsx
@@ -197,7 +197,7 @@ export default class ScrollPager<T extends Route> extends React.Component<
           onScroll={this.onScroll}
           onScrollBeginDrag={handleSwipeStart}
           onScrollEndDrag={handleSwipeEnd}
-          //onMomentumScrollEnd={this.onScroll}
+          onMomentumScrollEnd={this.onScroll}
           contentOffset={this.initialOffset}
           style={styles.container}
           contentContainerStyle={
